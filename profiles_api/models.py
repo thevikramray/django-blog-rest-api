fe93906eca85c7b0bfd958dd659303c7b140fd69
“vikramray” <vikram.ray@ezedox.com>
2019-09-28 13:45:25 +0530

    Created UserProfile Manager and added it as default user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
 # Create your models here.
+
+class ProfileUserManager(UserManager):
+    """ Profile manager Model """
+    def create_user(self, email, first_name, password=None):
+        if not email:
+            raise ValueError('You Must Provide an Email')
+        email = self.normalize_email
+        user = self.model(email=email, first_name=first_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, password=None):
+        """ Create a superUser , is_superuser comes from PermissionMixins """
+        user = create_user(email, first_name, password)
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class ProfileUser(AbstractBaseUser, PermissionsMixin):
+    """ Model to store Profile User """
+    """ This is custom user model ,add this in setting AUTH_USER_MODEL """
+    email = models.EmailField(max_length=128, unique=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128, blank=True)
+    mobile = models.CharField(max_length=13, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = ProfileUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    def get_full_name(self):
+        return '{first_name} {last_name}'.format(self.first_name, self.last_name)
+    
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.email

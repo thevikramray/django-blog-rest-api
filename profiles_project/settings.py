c3d3cb0f1e46f58ec6bd58161599b3c22e31aee8
“vikramray” <vikram.ray@ezedox.com>
2019-11-22 02:00:41 +0530

    added assets

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '*nu(mvx7#sh4!85pzz4%05$ybv%4hmk4kghaaw!&yr8-a7cd!l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('MY_DOMAIN'), os.environ.get('MY_DOMAIN2')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('MY_DOMAIN'), os.environ.get('MY_DOMAIN2')] 
 
 # Application definition
 
